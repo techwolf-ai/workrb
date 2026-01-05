e309675b7a8d576c1f150b0fe6866f44c99c6dd1
jjzha <mike.zhang@di.ku.dk>
2026-01-05 20:52:23 +0100

    refactor: add module export

--- src/workrb/tasks/ranking/__init__.py
+++ src/workrb/tasks/ranking/__init__.py
@@ -24,5 +24,6 @@ __all__ = [
     "HouseSkillExtractRanking",
     "JobBERTJobNormRanking",
     "SkillMatch1kSkillSimilarityRanking",
+    "SkillSkapeExtractRanking",
     "TechSkillExtractRanking",
 ]
