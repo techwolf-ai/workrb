9867830088bae207335ea4882fe4e94f0313a412
federetyk <fede.retyk@gmail.com>
2026-01-08 20:47:25 +0100

    feat: add lexical baselines for ranking

--- src/workrb/models/__init__.py
+++ src/workrb/models/__init__.py
@@ -6,12 +6,22 @@ from workrb.models.base import ModelInterface
 from workrb.models.bi_encoder import BiEncoderModel, ConTeXTMatchModel, JobBERTModel
 from workrb.models.classification_model import RndESCOClassificationModel
 from workrb.models.curriculum_encoder import CurriculumMatchModel
+from workrb.models.lexical_baselines import (
+    BM25Model,
+    EditDistanceModel,
+    RandomRankingModel,
+    TfIdfModel,
+)
 
 __all__ = [
+    "BM25Model",
     "BiEncoderModel",
     "ConTeXTMatchModel",
     "CurriculumMatchModel",
+    "EditDistanceModel",
     "JobBERTModel",
     "ModelInterface",
+    "RandomRankingModel",
     "RndESCOClassificationModel",
+    "TfIdfModel",
 ]
