72b8e40396749f2df489d83d94f24ad7dccdbd74
federetyk <fede.retyk@gmail.com>
2026-02-23 19:44:37 +0100

    test: make it explicit that the dataset key "en" comes from the Language enum

--- tests/test_freelancer_project_matching.py
+++ tests/test_freelancer_project_matching.py
@@ -4,15 +4,17 @@ from workrb.tasks.abstract.base import Language
 
 def test_freelancer_project_ranking_task_loads():
     """Test that task loads without errors"""
-    task = workrb.tasks.ProjectCandidateRanking(split="test", languages=[Language.EN.value])
-    dataset = task.datasets["en"]
+    dataset_name = Language.EN.value
+
+    task = workrb.tasks.ProjectCandidateRanking(split="test", languages=[dataset_name])
+    dataset = task.datasets[dataset_name]
 
     assert len(dataset.query_texts) > 0
     assert len(dataset.target_space) > 0
     assert len(dataset.target_indices) == len(dataset.query_texts)
 
-    task = workrb.tasks.SearchQueryCandidateRanking(split="test", languages=[Language.EN.value])
-    dataset = task.datasets["en"]
+    task = workrb.tasks.SearchQueryCandidateRanking(split="test", languages=[dataset_name])
+    dataset = task.datasets[dataset_name]
 
     assert len(dataset.query_texts) > 0
     assert len(dataset.target_space) > 0
